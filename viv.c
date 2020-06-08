3a16a052349c6d6021328a8e691e709ffa783ef3
vktiwary007 <vktiwary@asu.edu>
2020-06-08 12:33:20 -0700

    Turned off canonical mode

--- viv.c
+++ viv.c
@@ -3,25 +3,40 @@
  * Jun 8 2020		  *
  *			  *
  * Text Editor using C    *
-\* ***********************/
+\*************************/
 
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+/*
+ * Method to disable Raw Mod on exiting the editor
+ */
+void disableRawMode(){
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 
 /*
  * Method to enable raw mode in editor 
  */
-void enableRawMode(){
-	
-	//termios will get all the terminal attributes
-	struct termios raw;
-	//get terminal attributes and set it to raw
-	tcgetattr(STDIN_FILENO, &raw);
+void enableRawMode(){	
+	/* get terminal attributes and set it to orig_termios
+	 * termios will get all the terminal attributes
+	 */
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	//called while exiting the editor & disables Raw mode
+	atexit(disableRawMode);
+	struct termios raw = orig_termios;
+
 	/* flipped the bits of ECHO by applying bitwise NOT operator(~) 
 	 *  & updated the local flag.
+	 *  Turned off canonical mode by applying BITWISE NOT operator on ICANON flag.
+	 *  Now we'll be reading input byte-by-byte instead of line-by-line by updating
+	 *  ICANON flag
 	 */
-	raw.c_lflag &= ~(ECHO);
+	raw.c_lflag &= ~(ECHO | ICANON);
 	/* set the updated flag & TCSAFLUSH will apply the changes 
 	 * when all pending outputs are written to the terminal
 	 */
