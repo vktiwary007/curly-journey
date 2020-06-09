9c907419418a6a20bd2865a1d662d85f2d2ef60a
vktiwary007 <vktiwary@asu.edu>
2020-06-09 16:17:15 -0700

    Added few more flags to generate correct values of byte on keypress

--- viv.c
+++ viv.c
@@ -5,6 +5,8 @@
  * Text Editor using C    *
 \*************************/
 
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -29,14 +31,21 @@ void enableRawMode(){
 	//called while exiting the editor & disables Raw mode
 	atexit(disableRawMode);
 	struct termios raw = orig_termios;
-
+	/* IXON flag will prevent ctrl-S (XOFF) & ctrl-Q (XON) to pause data transmission
+	 * ICRNL will fix ctrl-M issue. CR: Carriage return, NL: New line
+	 */
+	raw.c_iflag &= ~(BRKINT | ICRNL |INPCK | ISTRIP | IXON);
+	// OPOST will turn off output processing features like "\n" to "\r\n"
+	raw.c_oflag &= ~(OPOST);
 	/* flipped the bits of ECHO by applying bitwise NOT operator(~) 
 	 *  & updated the local flag.
 	 *  Turned off canonical mode by applying BITWISE NOT operator on ICANON flag.
 	 *  Now we'll be reading input byte-by-byte instead of line-by-line by updating
-	 *  ICANON flag
+	 *  ICANON flag. ~ISIG flag will prevent ctrl-c to send SIGINT & 
+	 *  ctrl-z to send SIGSTP signal. ~IEXTEN will fix ctrl-V, ctrl-C, ctril-O behavior
 	 */
-	raw.c_lflag &= ~(ECHO | ICANON);
+	raw.c_cflag |=(CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 	/* set the updated flag & TCSAFLUSH will apply the changes 
 	 * when all pending outputs are written to the terminal
 	 */
@@ -47,7 +56,14 @@ int main(){
 	enableRawMode();
    	char c;
 	//reading input from standard keypress
-       	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');	
+       	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+		if(iscntrl(c)){
+			printf("%d\r\n", c);
+		}
+		else{
+			printf("%d ('%c')\r\n", c, c);
+		}
+	};	
 	return 0;
 }
 
